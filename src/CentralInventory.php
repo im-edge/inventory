6e92188e13018269af806641cafb5223907007b6
Thomas Gelf <thomas@gelf.net>
2024-10-27 17:09:26 +0100

    CentralInventory: drop loadTableSyncPositions

--- src/CentralInventory.php
+++ src/CentralInventory.php
@@ -8,6 +8,4 @@ interface CentralInventory
      * @param InventoryAction[] $actions
      */
     public function shipBulkActions(array $actions): void;
-
-    public function loadTableSyncPositions(NodeIdentifier $nodeIdentifier): array;
 }
